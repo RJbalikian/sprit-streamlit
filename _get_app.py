532d2a3a633a2a46ed2f9c871ddf62d24a31a1fd
RJ Balikian <rileybalikian@outlook.com>
2025-12-11 11:51:45 -0600

    allow quickly copying app from main sprit repository to here

--- _get_app.py
+++ _get_app.py
@@ -1,5 +1,10 @@
-app_url = r"https://raw.githubusercontent.com/RJbalikian/SPRIT-HVSR/main/sprit/sprit_streamlit_ui.py"
+channel='dev'
+
+app_url = f"https://raw.githubusercontent.com/RJbalikian/SPRIT-HVSR/{channel}/sprit/sprit_streamlit_ui.py"
 
 import requests
 r = requests.get(app_url)
 print(r.text)
+
+with open('./streamlit_app.py', mode='w', encoding='utf-8') as f:
+    f.write(r.text)
